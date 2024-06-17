837b01d6915e0c3f6c4c6a46cc9af2f8a2235fe3
abbasiiqra <abbasiiqra44@gmail.com>
2024-06-18 00:17:31 +0530

    quizquiz

---
+++ Iqra_20226066/Iqra_quiz/src/components/Quiz.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import {useNavigate,Link} from 'react-router-dom';
+
+export const Quiz = () => {
+  return (
+    <div className='container'>
+     <li> <Link to="/">Science</Link> </li>
+      
+    </div>
+  )
+}
+
+
+
