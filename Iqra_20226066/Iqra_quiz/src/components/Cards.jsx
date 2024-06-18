d833be6ba0b1ae6277ba0b69ec4cd58be868f1ca
abbasiiqra <abbasiiqra44@gmail.com>
2024-06-18 19:25:19 +0530

    xgwis

--- Iqra_20226066/Iqra_quiz/src/components/Cards.jsx
+++ Iqra_20226066/Iqra_quiz/src/components/Cards.jsx
@@ -1,127 +1,92 @@
-import React from 'react'
-import {TinderCard } from 'react-tinder-card';
-import './Cards.css'
+import React, { useState } from 'react';
+import TinderCard from 'react-tinder-card';
+import './Cards.css';
 import { useNavigate, useSearchParams } from 'react-router-dom';
-import { useState , useMemo} from 'react';
 
-  
-const ques ={science:[
-    {
-        Question: 'xyz',answer:true
-    },
-    {
-        Question: 'pqr', answer:false
-    }
-  
+const ques = {
+  science: [
+    { question: 'Iron is the most abundant metal in Earth’s crust.', answer: false },
+    { question: 'Helium gives off a pungent odor.', answer: false },
+    { question: 'The plants dinosaurs ate do not exist today.', answer: false },
+    { question: 'There are 500 stars visible to the naked eye from Earth.', answer: false },
+    { question: 'Silver is the most conductive of metals.', answer: true },
+    { question: 'Energy can be transmitted wirelessly.', answer: true },
+    { question: 'The speed of sound is constant.', answer: false },
+    { question: 'A meteor would burn up more rapidly in the atmosphere of Mars than it would passing through the Earth’s atmosphere.', answer: false },
+    { question: 'Water is the most common element on Earth.', answer: false },
+    { question: 'There are stars made of diamonds.', answer: true }
+
   ],
-  
-  popculture:[
-    {
-        Question: 'ktrjoij',answer:false
-    },
-    {
-        Question:'hgeiufho4w', answer:true
-    }
-  
+  popculture: [
+    { question: 'George Michael and Andrew Ridgeley were founding members of the 80s group Wham!', answer: true },
+    { question: 'Avengers stars Michael Keaton as a DC Comics superhero?', answer: false },
+    { question: 'Prince Charles and Princess Diana tied knot in 1981', answer: true },
+    { question: 'Launched in 1980, pac-man features four ghosts named Blinky, Pinky, Inky and Clyde', answer: true },
+    { question: 'Disney Land is the  Disney World theme park opened on May 1, 1989 ', answer: false },
+    { question: 'The 1984 Olympic Games were held in  Los Angeles', answer: true },
+    { question: 'vyihoerj', answer: true },
+    { question: 'vyihoerj', answer: true },
+    { question: 'vyihoerj', answer: true },
+    { question: 'vyihoerj', answer: true }
   ],
-  mythology:[
-    {
-        Question:'vyihoerj',answer:true
-    },
-    {
-        Question:'bhujroij', answer:true
-    }
+  mythology: [
+    { question: 'vyihoerj', answer: true },
+    { question: 'bhujroij', answer: true }
   ]
-  };
+};
 
-  export const Cards= () => {
-    const subject = localStorage.getItem("topic");
-    const currentQuestions = ques[subject] || [];
-
-    const navigate = useNavigate();
-
-    const [currentIndex, setCurrentIndex] = useState(0);
+export const Cards = () => {
+  const subject = localStorage.getItem("topic");
+  const currentQuestions = ques[subject] || [];
+  
+  const navigate = useNavigate();
+  const [currentIndex, setCurrentIndex] = useState(0);
     const [lastDirection, setLastDirection] = useState();
     const [score, setScore] = useState(0);
     
-
-    const childRefs = useMemo(
-        () => Array(currentQuestions.length).fill(0).map((i) => React.createRef()),
-        [currentQuestions.length]
-    );
     const updateCurrentIndex = (val) => {
         setCurrentIndex(val);
     };
-
-    const canGoBack = currentIndex < currentQuestions.length - 1;
     const canSwipe = currentIndex >= 0;
-
-    const swiped = (direction, question, index) => {
+    const swiped = (direction, ques, index) => {
         setLastDirection(direction);
+  if ((currentQuestions[currentIndex].answer === true && direction === "right") ||
+  (currentQuestions[currentIndex].answer === false && direction === "left"))
+{
+  setScore((prevScore) => prevScore + 1);
+}
 
-        if ((currentQuestions[index].answer === true && direction === "right") ||
-            (currentQuestions[index].answer === false && direction === "left")) {
-            setScore((prevScore) => prevScore + 1);
-        }
+ updateCurrentIndex((prevIndex) => prevIndex + 1);
+};
 
-        updateCurrentIndex((prevIndex) => prevIndex + 1);
-    };
+  const outOfFrame = (name) => {
+    console.log(name + ' left the screen!');
+  };
 
-    const outOfFrame = (name, idx) => {
-        console.log(`${name} (${idx}) left the screen!`);
-    };
-
-    const swipe = async (dir) => {
-        if (canSwipe && currentIndex < currentQuestions.length) {
-            await childRefs[currentQuestions.length-currentIndex-1].current.swipe(dir);
-        }
-    };
-
-    const goBack = async () => {
-        if (!canGoBack) return;
-        const newIndex = currentIndex-1;
-        updateCurrentIndex(newIndex);
-        await childRefs[newIndex].current.restoreCard();
-    };
-
-    return (
-        <div>
-            <div>
-                <button onClick={()=>{
-                    navigate("/")
-                }}
-                >End Quiz</button>
-                <button onClick={()=>{
-                     location.reload()
-                }}
-                >Retake Quiz</button>
+  return (
+    <div>
+      <h1>Quizzy</h1>
+      <div className='cardContainer'>
+        {currentQuestions.map((character) => (
+          <TinderCard
+            className='swipe'
+            key={character.question}
+            onSwipe={(dir) => swiped(dir, character.question)}
+            onCardLeftScreen={() => outOfFrame(character.question)}
+          
+            preventSwipe={['up', 'down']}>
+            <div className='card'>
+              <h3>{character.question}</h3>
             </div>
-            <h1>{subject?.toUpperCase()} QUIZ</h1>
-            <div className='cardContainer'>
-                {currentQuestions.map((character, index) => (
-                    <TinderCard
-                        ref={childRefs[index]}
-                        className='swipe'
-                        key={character.question}
-                        onSwipe={(dir) => swiped(dir, character.question, index)}
-                        onCardLeftScreen={() => outOfFrame(character.question, index)}
-                        preventSwipe={['up', 'down']}
-                    >
-                        <div className='card'>
-                            <h3>{character.question}</h3>
-                        </div>
-                    </TinderCard>
-                ))}
-            </div>
-            <div className='buttons'>
-                <button style={{ backgroundColor: !canSwipe && '#c3c4d3' }} onClick={() => swipe('left')}>Swipe left!</button>
-                <button style={{ backgroundColor: !canSwipe && '#c3c4d3' }} onClick={() => swipe('right')}>Swipe right!</button>
-            </div>
-                <h2 key={lastDirection} className='infoText'>
-                {Math.min(currentIndex,currentQuestions.length)} out of {currentQuestions.length}
-                </h2>
-            
-            <h2>Score: {score}</h2>
-        </div>
-    );
+          </TinderCard>
+        ))}
+      </div>
+      {lastDirection ? (
+        <h2 className='infoText'>You swiped {lastDirection}</h2>
+      ) : (
+        <h2 className='infoText' />
+      )}
+      <h3> Score:{score}</h3>
+    </div>
+  );
 };
